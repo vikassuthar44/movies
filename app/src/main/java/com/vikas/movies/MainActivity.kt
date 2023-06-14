1a5ac571f87b61be60a0f4766696164c220a75ef
vikassuthar <prsuthar44@gmail.com>
2023-06-14 22:17:27 +0530

    text update

--- app/src/main/java/com/vikas/movies/MainActivity.kt
+++ app/src/main/java/com/vikas/movies/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Hello Android Developer")
+                    Greeting(" Hello, Android Developer")
                 }
             }
         }
